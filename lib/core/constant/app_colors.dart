d35437265b33279b3c65449d0835de7dfcca67eb
mohand3del <101574221+mohand3del@users.noreply.github.com>
2024-12-15 01:31:47 +0200

    init project added colors file

---
+++ lib/core/constant/app_colors.dart
@@ -0,0 +1,10 @@
+
+
+import 'dart:ui';
+
+class AppColors {
+  static const Color darkColor = Color(0xFF181824);
+  static const Color primaryColor = Color(0xFF7562E0);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+}
